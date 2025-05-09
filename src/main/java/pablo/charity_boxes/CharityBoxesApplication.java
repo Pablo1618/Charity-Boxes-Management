8444d841857a16f9b6abe7fd385aec71708721a0
Pablo1618 <pawelwawrzynski22@gmail.com>
2025-05-09 12:40:40 +0200

    Initial Spring Initializr commit

---
+++ src/main/java/pablo/charity_boxes/CharityBoxesApplication.java
@@ -0,0 +1,14 @@
+package pablo.charity_boxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityBoxesApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(CharityBoxesApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/pablo/charity_boxes/CharityBoxesApplicationTests.java
@@ -0,0 +1,13 @@
+package pablo.charity_boxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityBoxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
